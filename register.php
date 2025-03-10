492adea8a80a3c336f0b3e6bdcea87a4f58bd444
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-03-10 23:06:30 +0600

    Initial Setup

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Login Page</title>
+    <title>Register Page</title>
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -46,8 +46,8 @@
 
     <div class="row mt-5">
         <div class="col-6 offset-md-3">
-            <div class="alert alert-success my-2" role="alert">
-                Login Success
+        <div class="alert alert-success my-2" role="alert">
+                Register Success
             </div>
             <div class="alert alert-danger my-2" role="alert">
                 Invalid Credentials!
@@ -63,7 +63,7 @@
                     <input type="password" class="form-control" id="exampleInputPassword1">
                 </div>
 
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit" class="btn btn-primary">Register</button>
             </form>
         </div>
     </div>
